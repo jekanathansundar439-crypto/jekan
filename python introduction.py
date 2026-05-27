47970b02b5915f66790b016c18b2b7860989a38e
jekanathansundar439-crypto <jekanathansundar439@gmail.com>
2026-05-27 11:01:38 +0530

    Add files via upload

---
+++ python introduction.py
@@ -0,0 +1,27 @@
+# PYTHON
+# Python is a high-level, interpreted programming language famous for its clear syntax and exceptional readability. 
+#      Created by Guido van Rossum and released in 1991, it focuses on helping developers write clean, 
+#      logical code for projects of any size.
+#👉 Python என்பது ஒரு high-level, interpreted programming language.இதைக் Guido van Rossum 1991-ல் உருவாக்கினார்.
+#👉 Python-ன் main aim என்னனா, programmers easy-a and clean-a code எழுத help பண்ணுவது. 
+#     Small project இருந்தாலும், big project இருந்தாலும் use பண்ணலாம்.
+
+# Key Features of Python
+# Easy to Read: Uses plain English keywords instead of complex punctuation.
+#👉 Python plain English words use பண்ணும்
+
+# Dynamically Typed: You do not need to declare variable types explicitly before using them.
+#👉 Variable type முன்னாடியே சொல்ல தேவையில்லை.
+
+# Significant Indentation: Uses spaces or tabs to define code blocks instead of curly brackets {}.
+#👉 Python curly braces {} use பண்ணாது.Spaces/tab use பண்ணி block define பண்ணும்.
+
+# Multi-Paradigm: Supports procedural, object-oriented, and functional programming models.
+#👉Python different programming styles support பண்ணும்.
+# Procedural Programming
+# Object-Oriented Programming
+# Functional Programming
+# அதாவது ஒரே language-ல பல method use பண்ணலாம்.
+
+# Extensive Libraries: Features a massive ecosystem of pre-built code packages for fast development
+#👉 Pythonக்கு already ready-made libraries ரொம்ப இருக்கு
